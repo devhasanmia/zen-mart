151453b44091c75ac8ba99fde4f057bb7cf3b570
devhasanmia <dev.hasanmia@gmail.com>
2024-11-29 17:59:48 +0600

    chore: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Login from "./pages/Login"
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+const Login = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello Login
+            </h1>
+        </div>
+    )
+}
+
+export default Login
